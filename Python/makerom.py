d84977e056da9031314217608e139cde028eb17c
RobbeCandries <94365598+RobbeCandries@users.noreply.github.com>
2024-04-16 22:08:06 +0200

    Update makerom.py
    Added a program that binks led's on the B-port of the 65c22

--- Python/makerom.py
+++ Python/makerom.py
@@ -1,4 +1,24 @@
-rom = bytearray([0xff] * 0x20000)
+# Make code array
+code = bytearray([
+    0xa9, 0xff,         # lda #$ff
+    0x8d, 0x02, 0x60,   # sta $6002
+
+    0xa9, 0x55,         # lda #$55
+    0x8d, 0x00, 0x60,   # sta $6000
+
+    0xa9, 0xaa,         # lda #$aa
+    0x8d, 0x00, 0x60,   # sta $6000
+
+    0x4c, 0x05, 0x80    # jmp $8005
+])
+
+
+# Make ROM array with NOP instruction and add code array to ROM array
+rom = code + bytearray([0xea] * (0x20000 - len(code)))
+
+# Reset Vector
+rom[0x7ffc] = 0x00
+rom[0x7ffd] = 0x80
 
 with open('rom.bin', 'wb') as out_file:
     out_file.write(rom)
